1efa01c5f52d3f378c95dfa281dfb25652f6c81e
Marine Delfosse <re-mamarane@hotmail.fr>
2024-10-16 18:53:52 +0200

    Style home

--- planning-app-project/src/pages/Home.jsx
+++ planning-app-project/src/pages/Home.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Header from '../components/Header'
 import Navigation from '../components/Navigation'
+import '../styles/home.scss'
 
 function Home() {
   return (
     <div>
         <Header />
         <Navigation />
-        <h1>Org-App</h1>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, cupiditate excepturi architecto magnam dolore ea deserunt id. Consectetur natus, iste, dolores voluptatem omnis modi similique pariatur ullam delectus, neque vitae?
-        </p>
+        <div className='home'>
+          <h1>Org-App</h1>
+          <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, cupiditate excepturi architecto magnam dolore ea deserunt id. Consectetur natus, iste, dolores voluptatem omnis modi similique pariatur ullam delectus, neque vitae?
+          </p>
+        </div>
     </div>
   )
 }
