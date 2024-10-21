554a2812198506a99dd6e54f41b21896af148cbd
Marine Delfosse <re-mamarane@hotmail.fr>
2024-10-21 16:08:53 +0200

    Ajout de l'affichage des cases en fonction des jours de la semaine

--- planning-app-project/src/components/Grid.jsx
+++ planning-app-project/src/components/Grid.jsx
@@ -3,32 +3,46 @@ import '../styles/components/grid.scss'
 
 
 
-function Grid({ currentMonth, setCurrentMonth, currentYear, setCurrentYear }) {
+function Grid({ currentMonth, currentYear }) {
 
-  // Génère un tableau des jours du mois
-  const [grid, setGrid] = useState([])
+  const [grid, setGrid] = useState([])      // Génère un tableau des jours du mois
 
-  // Nombre de jour par mois
-  const getDaysInMonth = (year, month) => {
-    return new Date(year, month + 1, 0).getDate()
+  const getDaysInMonth = (year, month) => new Date(year, month + 1, 0).getDate()
+
+  const getFirstDayInMonth = (year, month) => {
+    let today = new Date(year, month, 1).getDay()
+    let goodDay
+    today === 0 ? goodDay = 6 : goodDay = today -1
+    return goodDay
   }
+
 
   // Calcule et met à jour la grille des jours du mois actuel à chaque fois que l'année (currentYear) ou le mois (currentMonth) change
   // Génère un tableau contenant ts les jours du mois et met à jour l'état "grid" avec ce tableau => permet de re-rendre l'interface avec le bon nombre de jours
   useEffect(() => {
     const daysInMonth = getDaysInMonth(currentYear, currentMonth)
+    let firstDay = getFirstDayInMonth(currentYear, currentMonth)
     let newGrid = []
+
+    // Ajoute des cases vides au début pour aligner le premier jour du mois
+    for (let j = 0; j < firstDay; j++) {
+      newGrid.push("")
+    }
+
+    // Ajoute les jours du mois
     for (let i = 1; i <= daysInMonth; i++) {
       newGrid.push(i)
     }
+
     setGrid(newGrid)
+
   }, [currentYear, currentMonth])   // [currentYear, currentMonth], indique que cet effet doit se ré-exécuter à chaque fois que currentYear ou currentMonth change
 
-  // Parcours du tableau
-  const browseGrid = () => {
-    return grid.map((day, index) => <div key={index} className='day'>{day}</div>)
-  }
 
+  // Parcours du tableau
+  const browseGrid = () => grid.map((day, index) => <div key={index} className='day'>{day}</div>)
+
+  
   return (
     <div className='grid'>
       {browseGrid()}
