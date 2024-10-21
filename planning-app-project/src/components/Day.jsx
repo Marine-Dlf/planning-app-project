2f43f697637d36ff14c6db0ffabcaee10dfe55ef
Marine Delfosse <re-mamarane@hotmail.fr>
2024-10-21 11:16:13 +0200

    Création des cases (jours) du calendrier (données en dures)

--- planning-app-project/src/components/Day.jsx
+++ planning-app-project/src/components/Day.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 function Day() {
   return (
     <div>
-      <p>Je suis une case</p>
+      
     </div>
   )
 }
+
+
 
 export default Day
--- planning-app-project/src/components/Grid.jsx
+++ planning-app-project/src/components/Grid.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import '../styles/components/grid.scss'
 
-const grid = []
+const grid = ['hello', 'coucou', 'ciao', 'hola', 'konichiwa', 'salut', 'bonjour', 'yeah']
+
 
 function Grid() {
+
   return (
-    <div>
-      {grid}
+    <div className='grid'>
+      {browseGrid()}
     </div>
   )
 }
+
+// Parcours du tableau
+const browseGrid = () => {
+  return grid.map((day, index) => <div key={index} className='day'>{day}</div>)
+}
+
 
 export default Grid
