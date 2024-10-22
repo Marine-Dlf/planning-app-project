c17e86904d2410b9f6da12809eb0c3c40b39e920
Marine Delfosse <re-mamarane@hotmail.fr>
2024-10-22 17:49:30 +0200

    Début de la création de la page connexion et de son style

--- planning-app-project/src/pages/Connexion.jsx
+++ planning-app-project/src/pages/Connexion.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import '../styles/connexion.scss'
+
 
 function Connexion() {
   return (
-    <div>
-      <h1>Connexion</h1>
+    <div className='parent'>
+      <div className='connexion'>
+        <h1>Connexion</h1>
+        <form>
+          <label>Pseudo</label>
+          <input type='text'/>
+          <label>E-mail</label>
+          <input type='text'/>
+          <label>Mot de Passe</label>
+          <input type='text'/>
+          <button type='submit'>Connexion</button>
+        </form>
+      </div>
     </div>
   )
 }
