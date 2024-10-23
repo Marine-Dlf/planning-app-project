85ebd9a330f9e2d8c84495dc37fccf933abc9f56
Marine Delfosse <re-mamarane@hotmail.fr>
2024-10-23 17:35:39 +0200

    Ajout de la déco du bouton se déconnecter dans le Header

---
+++ planning-app-project/src/components/DisconnectButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DisconnectButton() {
+  return (
+    <div className='disconnect'>
+      Se déconnecter
+    </div>
+  )
+}
+
+export default DisconnectButton
--- planning-app-project/src/components/Header.jsx
+++ planning-app-project/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../styles/components/header.scss'
+import DisconnectButton from './DisconnectButton'
 
 function Header() {
   return (
     <div className='header'>
       <p>Org-App</p>
+      <DisconnectButton />
     </div>
   )
 }
