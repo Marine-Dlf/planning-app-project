eb00d7f82877296de8a25556f190848bdba0ed9f
Marine Delfosse <re-mamarane@hotmail.fr>
2024-10-24 15:59:55 +0200

    Changement de format de la date dans la popup jour

--- planning-app-project/src/components/Popup.jsx
+++ planning-app-project/src/components/Popup.jsx
@@ -9,11 +9,17 @@ function Popup({ type, day, currentMonth, currentYear, onClose }) {
     let content;
     
     if (type === 'day') {
-        const formatedDate = new Date(currentYear, currentMonth, day).toLocaleDateString('fr-FR')
+        // const formatedDate = new Date(currentYear, currentMonth, day).toLocaleDateString('fr-FR')        // Si je préfère afficher ??/??/????
+        const date = new Date(currentYear, currentMonth, day)
+        const dayOfWeek = date.toLocaleDateString('fr-FR', { weekday: 'long' })
+        const dayOfWeekCapital = dayOfWeek.charAt(0).toUpperCase() + dayOfWeek.slice(1)     // ==> Majuscule à la 1ère lettre du jour
+        const nameOfMonth = date.toLocaleDateString('fr-FR', { month: 'long'})
+
         content = <div>
-            <p className='todaysDate'>{formatedDate}</p>
+            <p className='todaysDate'>{dayOfWeekCapital} {day} {nameOfMonth} {currentYear}</p>
             <Form />
         </div>
+
     } else if (type === 'month') {
         content = <p>- Affichage du mois en cours: {monthName[currentMonth]}<br/>et des 11 autres mois<br/>- Format: un rectangle de 3 mois en largeur et 4 en hauteur</p>
     } else if (type === 'year') {
@@ -25,7 +31,7 @@ function Popup({ type, day, currentMonth, currentYear, onClose }) {
         <div className='popupBackground'>
             <div className='popup'>
                 <div className='closeButtonDiv'>
-                    <button className='closeButton' onClick={onClose}>❌</button>
+                    <button className='closeButton' onClick={onClose}>×</button>
                 </div>
                 <div className='popupContent'>
                     {content}
