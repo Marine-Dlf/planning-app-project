cc95e3ca5179d8a129d64dbf73be9509e1bfff0b
Marine Delfosse <re-mamarane@hotmail.fr>
2024-11-05 16:33:11 +0100

    fetch API et affichage des données de la bdd dans les cases correspondantes. Installation de axios mais pas utilisation pour l'instant

--- planning-app-project/src/components/Day.jsx
+++ planning-app-project/src/components/Day.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
-function Day({ day, onClick }) {
+function Day({ day, onClick, events }) {
   return (
     <div
       className={'day' + (day === "" ? " emptyCase" : "")}
       onClick={onClick}
     >
-    {day !== "" ? day : ""}
+    
+    {day !== '' ? (
+        <>
+          <p>{day}</p>
+          {events.map((event, index) => (
+            <div key={index} className='event'>
+              {event.name} {/* Affichez le nom de l'événement ou d'autres détails */}
+            </div>
+          ))}
+        </>
+      ) : (
+        ''
+      )}
+      
     </div>
   )
 }
--- planning-app-project/src/components/Grid.jsx
+++ planning-app-project/src/components/Grid.jsx
@@ -6,14 +6,35 @@ import Day from './Day';
 function Grid({ currentMonth, currentYear, displayPopup }) {
 
   const [grid, setGrid] = useState([]);      // Génère un tableau des jours du mois
+  const [events, setEvents] = useState([])   // État pour stocker les événements
 
 
   const getDaysInMonth = (year, month) => new Date(year, month + 1, 0).getDate()
+
 
   const getFirstDayInMonth = (year, month) => {
     let today = new Date(year, month, 1).getDay()
     return today === 0 ? 6 : today - 1
   }
+
+
+  // Fetch pour ensuite pouvoir afficher les données
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/events/');
+        if (!response.ok) {
+          // Vérifier si la réponse est OK (status 200-299) (si ce n'est pas le cas, il lève une erreur)
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();         // Si la réponse est correcte, on extrait les données JSON de la réponse.
+        setEvents(data);                            // Stocker les événements dans l'état
+      } catch (error) {
+        console.error('Erreur lors de la récupération des événements :', error);
+      }
+    }
+    fetchEvents()
+}, [currentMonth, currentYear]);                    // Appel de l'API à chaque changement de mois ou d'année
 
 
   // Calcule et met à jour la grille des jours du mois actuel à chaque fois que l'année (currentYear) ou le mois (currentMonth) change
@@ -39,13 +60,24 @@ function Grid({ currentMonth, currentYear, displayPopup }) {
 
 
   // Parcours du tableau
-  const browseGrid = () => grid.map((day, index) => (
+  const browseGrid = () => grid.map((day, index) => {
+
+    const dayEvents = events.filter(                            // Filtre les events pr obtenir uniquement ceux qui correspondent au jour en cours
+      (event) =>                                                // puis, event.date est converti en Date
+        new Date(event.date).getFullYear() === currentYear &&   // et on vérifie si l'année,   
+        new Date(event.date).getMonth() === currentMonth &&     // le mois 
+        new Date(event.date).getDate() === day                  // et le jour de cet événement correspondent à currentYear, currentMonth et day
+    );
+
+    return (
     <Day
       key = {index}
-      day = {day}     // Passe le jour ou la case vide au composant Day
+      day = {day}             // Passe le jour ou la case vide au composant Day
+      events = {dayEvents}    // Passe les évènements de ce jour au composant Day
       onClick = {day ? () => displayPopup('day', day) : undefined}     // Passe l'action de clic au composant Day
     />
-  ))
+    )
+  })
 
   
   return (
