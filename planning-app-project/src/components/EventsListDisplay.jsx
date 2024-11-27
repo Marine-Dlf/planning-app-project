952ddf00f05c7b328ccb50c9e19dc85a0b143245
Marine Delfosse <re-mamarane@hotmail.fr>
2024-11-27 12:15:50 +0100

    Formatage de la date au format français dans EventsListDisplay

--- planning-app-project/src/components/EventsListDisplay.jsx
+++ planning-app-project/src/components/EventsListDisplay.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import '../styles/components/eventsListDisplay.scss'
 
 
-function EventsListDisplay({ sortedEvents, type, types }) {
+function EventsListDisplay({ sortedEvents, types }) {
 
     console.log(sortedEvents)
+
     return (
         <div>
             <h3>Liste de vos évènements</h3>
@@ -14,9 +15,11 @@ function EventsListDisplay({ sortedEvents, type, types }) {
                     const type = types.find(type => type.id === sortedEvent.types_id);
                     const typeName = type ? type.typeName : "Type inconnu";
 
+                    const formattedDate = new Intl.DateTimeFormat('fr-FR').format(new Date(sortedEvent.date));
+
                     return (
                         <li key={index} className={`item ${typeName}`}>
-                            <p className='date'>{sortedEvent.date}</p>
+                            <p className='date'>{formattedDate}</p>
                             <p className='time'>{sortedEvent.time}</p>
                             <p className='eventName'>{sortedEvent.eventName}</p>
                         </li>
