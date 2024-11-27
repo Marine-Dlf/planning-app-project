1c42e5abe60122f42284357c6d6e03dbacb11a21
Marine Delfosse <re-mamarane@hotmail.fr>
2024-11-27 17:32:39 +0100

    EventsListDisplay: scroll automatique de la liste d'event jusqu'à l'évènement courant/prochain (useRef - useEffect)

--- planning-app-project/src/components/EventsListDisplay.jsx
+++ planning-app-project/src/components/EventsListDisplay.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import '../styles/components/eventsListDisplay.scss'
 
 
 function EventsListDisplay({ sortedEvents, types }) {
 
-    console.log(sortedEvents)
+    const listRef = useRef(null);           // Ref for list: creating a reference 'listRef'
+    const eventRefs = useRef([]);           // Ref for each event
+
+
+    // Find the index of the next event or today's event
+    const currentDateTime = new Date();
+    const nextEventIndex = sortedEvents.findIndex(event => new Date(event.date) >= currentDateTime); // trouve l'index du premier événement dont la date est égale ou supérieure à la date et l'heure actuelles
+
+
+    useEffect(() => {
+        // Scroll to current/next event if found (auto scroll)
+        if (nextEventIndex !== -1 && eventRefs.current[nextEventIndex]) {
+            eventRefs.current[nextEventIndex].scrollIntoView({
+                behavior: 'smooth',
+                block: 'center',
+            });
+        }
+    }, [nextEventIndex]);
+
+    // Each item in the list has its own ref, allowing you to scroll directly to a specific item
+
 
     return (
         <div>
             <h3>Liste de vos évènements</h3>
-            <ul className='listItem'>
+
+            <ul className='listItem' ref={listRef}>
+                
                 {sortedEvents.map((sortedEvent, index) => {
 
                     const type = types.find(type => type.id === sortedEvent.types_id);
@@ -21,12 +43,24 @@ function EventsListDisplay({ sortedEvents, types }) {
 
 
                     return (
-                        <li key={index} className={`item ${typeName}`}>
-                            <p className='date'><strong>{formattedDate}</strong> ({dayOfWeek}) </p>
+                        <li
+                            key={index}
+                            className={`item ${typeName}`}
+                            ref={el => (eventRefs.current[index] = el)}         // Assigns each value to a ref
+                        >
+
+                            <p className='date'>
+                                <strong>{formattedDate}</strong> ({dayOfWeek})
+                            </p>
+
                             {sortedEvent.time ? (
-                                <p className='time'>{new Date(`1970-01-01T${sortedEvent.time}`).toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })}</p>
+                                <p className='time'>
+                                    {new Date(`1970-01-01T${sortedEvent.time}`).toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })}
+                                </p>
                             ) : null}
+
                             <p className='eventName'>{sortedEvent.eventName}</p>
+
                         </li>
                     )
                 })}
